3bb9c851cce4eb289baaaf3f2072045e313652de
Levan Beroshvili <lberoshvili9@gmail.com>
2026-03-15 17:49:28 +0400

    Test change 2

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,7 +3,7 @@ import type { AboutPageProps } from '@shopit/template-sdk';
 export function AboutPage({ locale, subdomain }: AboutPageProps) {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>About Us Page</h1>
+      <h1>About Us</h1>
       <p>Welcome to {subdomain}</p>
     </div>
   );
